11ac2ce890d18b652de86691ef11d20986f5910f
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-14 12:01:59 +0000

    Update api url

--- src/helpers/ApiCallHelper.tsx
+++ src/helpers/ApiCallHelper.tsx
@@ -1,5 +1,5 @@
 export const fetchStations = () => {
-    return fetch('https://mobile-api-softwire2.lner.co.uk/v1/stations', {
+    return fetch('https://int-dev2.tram.softwire-lner-dev.co.uk/v1/stations', {
         headers: {
             'X-API-KEY': `${process.env.REACT_APP_X_API_KEY}`,
         },
